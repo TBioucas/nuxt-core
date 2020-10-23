84b6cc69d1767a20beece931e13408ba7901f7d3
TBioucas <tiago.bioucas@gmail.com>
2020-10-23 23:51:41 +0100

    Setup files;

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/no-v-html': 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/no-unused-components': 'error',
+  },
+  globals: {
+    $nuxt: true,
+  },
+};
